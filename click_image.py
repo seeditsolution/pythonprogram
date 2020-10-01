748d3b6dc17303b258195bb48a5e04996c32c1c2
Anisha Mamgain <mamgainanisha@gmail.com>
2020-10-01 19:05:46 +0530

    Click images

---
+++ click_image.py
@@ -0,0 +1,26 @@
+#https://www.facebook.com/anisha.mamgain.37/posts/1061681344253497
+#Subscribed by Code House
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+img = 0
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("Image", frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        # ESC pressed
+        print("Closing the window...")
+        break
+    elif k == 32:
+        # Press SPACE to click picture
+        img_name = "opencv_frame.jpg".format(img)
+        cv2.imwrite(img_name, frame)
+        print(format(img_name))
+        img += 1
+
+cap.release()
+
+cv2.destroyAllWindows()
