568148b6465bdcb68e1dc8edf222cbd6e8f2bed7
AdnanSE <72250612+AdnanSE@users.noreply.github.com>
2020-10-02 13:35:19 +0500

    type-conversion-with-the-int-float-and-str-functions

---
+++ typeConversion.py
@@ -0,0 +1,18 @@
+print(int(3.14))
+print(int(3.99))
+print(int("3"))
+print(int(3))
+
+print(float(5))
+print(float("10.32"))
+print(float(5.23))
+
+print(str(5.35))
+print(str(5))
+print(str("Hello"))
+
+print(str(5) + "Hello")
+
+print(3 + 3)
+print(4.3 + 5.7)
+print(3 + 5.8)
