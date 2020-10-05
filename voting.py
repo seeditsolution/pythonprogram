04e3aa004b12d46b686425a1cd740ec0b4e245b5
Abhishek1334 <72259451+Abhishek1334@users.noreply.github.com>
2020-10-05 14:34:02 +0530

    Create voting.py

---
+++ voting.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter your marks out of 100 = "))
+if marks >=80:
+    print("A")
+elif marks>=60:
+    print("B")
+elif marks>=40:
+    print("C")
+else:
+    print("BETTER LUCK NEXT TIME")
+print("THANKS")
